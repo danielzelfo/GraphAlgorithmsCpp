1b796446319fd4bd40d432b891035c2398f494ac
danielzelfo <daniel.zelfo@gmail.com>
2020-06-08 21:15:36 -0700

    fixed algorithm

--- TopologicalOrdering.h
+++ TopologicalOrdering.h
@@ -7,27 +7,31 @@
 using namespace std;
 
 class TopologicalOrdering : public DFS {
-
+	int n;
+	
+public:
 
 	void startVisit(Vertex<string>* v) {
-		if (!isVisited(v))
-			cout << v->getElement() << " ";
-	}
+		if (!isVisited(v)) {
+			bool g = true;
+			vector<Edge<int>*> inc = graph->incomingEdges(v);
+			for (int i = 0; i < inc.size(); ++i)
+				if (!isVisited(graph->opposite(v, inc[i]))) {
+					dfsTraversal(graph->opposite(v, inc[i]));
+					g = false;
+				}
 
-	void traverseDiscovery(Edge<int>* e, Vertex<string>* from) {
-		vector<Edge<int>*> inc = graph->incomingEdges(graph->opposite(from, e));
-		for (int i = 0; i < inc.size(); ++i)
-			if (!isVisited(graph->opposite(graph->opposite(from, e), inc[i]))){
-				dfsTraversal(graph->opposite(graph->opposite(from, e), inc[i]));
-				return;
-			}
+			if(g)
+				cout << v->getElement() << " ";
+		}
 			
+		
 	}
-	void finishVisit(Vertex<string>* v) {
-		visit(v);
+
+	bool isDone() {
+		return n >= graph->getVertices().size();
 	}
 
 
-public:
     TopologicalOrdering(Graph<string, int>* g) : DFS(g) { }
 };
